1d0e48301d837df8395ab1f7e3cdbbf5a54317be
AlejandroJafet <45410208+AlejandroJafet@users.noreply.github.com>
2018-11-29 13:36:22 -0600

    Cambio boton Acceder

--- login/index.php
+++ login/index.php
@@ -42,7 +42,7 @@ if(isset($_SESSION['session']))
 				</form>
 			</div><!--fin cuerpo-->
 	
-			<div id="pie">INFTEC</div>
+			<div id="pie">ACCEDER</div>
 		</div><!-- fin contenedor -->
 
 	</div><!--fin envoltura-->
