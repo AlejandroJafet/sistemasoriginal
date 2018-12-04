4c9ad91e08fb823fa6d7088f8d69820011b7b5d1
AlejandroJafet <45410208+AlejandroJafet@users.noreply.github.com>
2018-12-04 09:50:26 -0600

    Cambio de usuario a miembro

--- login/index.php
+++ login/index.php
@@ -36,7 +36,7 @@ if(isset($_SESSION['session']))
 			<div id="cuerpo">
 				<form id="form-login" action="./php/login.php" method="post" autocomplete="off">
 
-					<p><label >USUARIO:</label></p>
+					<p><label >Miembro:</label></p>
 						<input name="usuario" type="text" id="usuario" placeholder="Ingresa Usuario" autofocus="" required=""></p>
 					
 					<p><label>Pin:</label></p>
